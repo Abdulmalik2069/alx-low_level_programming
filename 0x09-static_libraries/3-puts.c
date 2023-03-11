abb9143524b30671d3de4ef279eb0d4a88f3ebbd
Abdulmalik2069 <abdulmaliklatunde123@gmail.com>
2023-03-10 16:18:23 -0800

    0x09-static_libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -10,8 +10,9 @@ void _puts(char *s);
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*s == '\0')
+	_putchar(*s++);
+	_putchar('\n');
 }
